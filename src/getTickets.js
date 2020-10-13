c0cf7933d22329caaad8719cdf9bbacb86872189
Ryan Welcher <me@ryanwelcher.com>
2020-10-13 15:02:36 -0400

    Add mock tickets for development env. (#1)

--- src/getTickets.js
+++ src/getTickets.js
@@ -1,15 +1,21 @@
 const getGutenbergTickets = require('./github');
 const getTracTickets = require('./trac');
+const mockTickets = require('../test/mock-tickets.json');
 
 module.exports = async function getTickets() {
 	const tweetsToSend = {};
-	const [trac, gb] = await Promise.all([getTracTickets(), getGutenbergTickets()]);
-	const tickets = [...trac, ...gb];
+	let tickets;
+	if (process.env.NODE_ENV === 'production') {
+		const [trac, gb] = await Promise.all([getTracTickets(), getGutenbergTickets()]);
+		tickets = [...trac, ...gb];
+	} else {
+		tickets = mockTickets;
+	}
+
 	const tweetKeys = Object.keys(tweetsToSend);
 	tickets.forEach((ticket) => {
 		const { issue, url, title, type } = ticket;
 		if (!tweetKeys.some((key) => key === type + issue)) {
-			console.log(`Adding ${type}${issue} to queue`);
 			tweetsToSend[type + issue] = {
 				type,
 				issue,
