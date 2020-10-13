0050e8b5982b131dcfc6773da887e7701c80db89
Ryan Welcher <ryan.welcher@10up.com>
2020-10-13 14:26:30 -0400

    Add getLatestMentions.

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 /**
  * Created by ryanwelcher on 2017-04-07.
  */
+require('dotenv').config();
+
+const TwitterPackage = require('twitter');
+
+const Twitter = new TwitterPackage({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 
 const getTickets = require('./src/getTickets');
 const sendATweet = require('./src/sendATweet');
+const getLatestMentions = require('./src/getLatestMentions');
 
 let firstRun = true;
+let lastRespondedToTweet;
 const HALF_HOUR = 30 * 60 * 1000;
 const FIFTEEN_MINUTES = HALF_HOUR / 2;
+const HOUR = HALF_HOUR * 2;
+const { STARTING_TWEET } = process.env;
 
 (async function () {
 	let tweetsToSend = await getTickets();
+	let mentions;
 	if (firstRun) {
+		mentions = await getLatestMentions(STARTING_TWEET);
 		console.log('Auto tweeting on first run');
 		sendATweet(tweetsToSend);
 		firstRun = false;
@@ -24,4 +40,48 @@ const FIFTEEN_MINUTES = HALF_HOUR / 2;
 	setInterval(() => {
 		sendATweet(tweetsToSend);
 	}, FIFTEEN_MINUTES);
+
+	setInterval(async () => {
+		mentions = await getLatestMentions(lastRespondedToTweet);
+		mentions.forEach((mention) => {
+			const {
+				text,
+				id: replyToTweet,
+				user: { screen_name: username },
+			} = mention;
+			if (username !== 'ryanwelcher') {
+				const handle = `@${username}`;
+				const lowerCaseText = text.toLowerCase();
+				console.log(lowerCaseText);
+				if (lowerCaseText.includes('need a ticket')) {
+					// Pick a random tweet
+					const tweetKeys = Object.keys(tweetsToSend);
+					if (tweetKeys.length !== 0) {
+						const index = tweetKeys[Math.floor(Math.random() * tweetKeys.length)];
+						const { issue, title, type, url } = tweetsToSend[index];
+						const hashtags =
+							type === 'gb' ? '#GoodFirstBug #Gutenberg' : '#GoodFirstBug #WordPress';
+						const message = `Here you go ${handle}! #${issue}: ${title} ${url} ${hashtags}`;
+						lastRespondedToTweet = replyToTweet;
+						if (process.env.NODE_ENV === 'production') {
+							Twitter.post(
+								'statuses/update',
+								{ status: message, in_reply_to_status_id: replyToTweet },
+								function (err) {
+									if (message) {
+										console.log(`Responded: ${message}`);
+									}
+									if (err) {
+										console.log(err);
+									}
+								},
+							);
+						} else {
+							console.log(`Dev: Responded: ${message}`);
+						}
+					}
+				}
+			}
+		});
+	}, HOUR);
 })();
