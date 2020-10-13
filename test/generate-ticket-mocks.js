385059f28a9814c0ab92353d3e9d91f91c39d8cc
Ryan Welcher <ryan.welcher@10up.com>
2020-10-13 14:57:20 -0400

    Add mock tickets for development env.

---
+++ test/generate-ticket-mocks.js
@@ -0,0 +1,21 @@
+/**
+ * Node dependencies
+ */
+const fs = require('fs');
+
+/**
+ * Internal dependencies
+ */
+const getGutenbergTickets = require('../src/github');
+const getTracTickets = require('../src/trac');
+
+(async function () {
+	const [trac, gb] = await Promise.all([getTracTickets(), getGutenbergTickets()]);
+	const tickets = [...trac, ...gb];
+	console.log(tickets);
+	fs.writeFile('./mock-tickets.json', JSON.stringify(tickets), function (err) {
+		if (err) return console.log(err);
+		console.log('Ticket mocks created');
+		return true;
+	});
+})();
