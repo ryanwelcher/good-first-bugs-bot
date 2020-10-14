e6b4998b0778eeec4f5610274b70b624cb8e685d
Ryan Welcher <me@ryanwelcher.com>
2020-10-13 23:14:00 -0400

    feature/create message utils (#5)
    * Adds Jest setup.
    
    * Add message relate utilites and test.
    
    * Use generateMessage util.
    
    * Rename utility to better reflect usage.

---
+++ src/utils/tweet-status.test.js
@@ -0,0 +1,40 @@
+/**
+ * Internal dependencies
+ */
+const { generateTweetStatus, generateHashTags, hashTags } = require('./tweet-status');
+
+describe('generateTweetStatus', () => {
+	it('generates tweet message for Gutenberg ticket', () => {
+		const ticket = {
+			issue: '12345',
+			title: 'Test GB Ticket',
+			type: 'gb',
+			url: 'https://www.example.com/test-ticket',
+		};
+		const hashtags = hashTags[ticket.type].join(' ');
+		expect(generateTweetStatus(ticket)).toBe(
+			`#${ticket.issue}: ${ticket.title} ${ticket.url} ${hashtags}`,
+		);
+	});
+	it('generates tweet message for trac ticket', () => {
+		const ticket = {
+			issue: '12345',
+			title: 'Test WordPress trac ticket',
+			type: 'trac',
+			url: 'https://www.example.com/test-ticket',
+		};
+		const hashtags = hashTags[ticket.type].join(' ');
+		expect(generateTweetStatus(ticket)).toBe(
+			`#${ticket.issue}: ${ticket.title} ${ticket.url} ${hashtags}`,
+		);
+	});
+});
+
+describe('generateHashTags', () => {
+	it('generates Gutenberg hash tags', () => {
+		expect(generateHashTags('gb')).toBe('#GoodFirstBug #Gutenberg');
+	});
+	it('generates WordPress trac hash tags', () => {
+		expect(generateHashTags('trac')).toBe('#GoodFirstBug #WordPress');
+	});
+});
